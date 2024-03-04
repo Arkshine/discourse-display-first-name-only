17ed0287499f3c1866398ef2642a1bf1279a10f7
Arkshine <github@arkshine.me>
2024-03-04 17:22:14 +0100

    DEV: Remove debug

--- javascripts/discourse/api-initializers/display_first_name_only.js
+++ javascripts/discourse/api-initializers/display_first_name_only.js
@@ -3,23 +3,12 @@ import User from "discourse/models/user";
 import { observes } from "discourse-common/utils/decorators";
 
 export default apiInitializer("1.8.0", (api) => {
-  if (settings.enable_debug) {
-    console.log("> DEBUG: display-first-name-only");
-  }
-
   // Prefilling the username based on the first name.
   api.modifyClass("component:modal/create-account", {
     pluginId: "display-first-name-only",
 
     init() {
       this._super(...arguments);
-
-      if (settings.enable_debug) {
-        console.log(" component:modal/create-account");
-        console.log("  - skipConfirmation:", this.model.skipConfirmation);
-        console.log("  - hasAuthOptions:", this.hasAuthOptions);
-        console.log("  - accountUsername:", this.model.accountUsername);
-      }
 
       if (
         this.model.skipConfirmation ||
@@ -36,10 +25,6 @@ export default apiInitializer("1.8.0", (api) => {
       }
 
       User.checkUsername(firstName, this.accountEmail).then((result) => {
-        if (settings.enable_debug) {
-          console.log("  checkUsername, result:", result.suggestion);
-        }
-
         if (result.suggestion) {
           this.setProperties({
             accountUsername: result.suggestion,

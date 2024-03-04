8ae62b9bd825e5bf2ba1a5d07613a83fe1b7117e
Arkshine <github@arkshine.me>
2024-03-04 15:41:06 +0100

    DEV: Add debug message

--- javascripts/discourse/api-initializers/display_first_name_only.js
+++ javascripts/discourse/api-initializers/display_first_name_only.js
@@ -8,12 +8,23 @@ export default apiInitializer("1.8.0", (api) => {
     return username.replace(/\d+$/, "");
   });
 
+  if (settings.enable_debug) {
+    console.log("> DEBUG: display-first-name-only");
+  }
+
   // Prefilling the username based on the first name.
   api.modifyClass("component:modal/create-account", {
     pluginId: "display-first-name-only",
 
     init() {
       this._super(...arguments);
+
+      if (settings.enable_debug) {
+        console.log(" component:modal/create-account");
+        console.log("  - skipConfirmation:", this.model.skipConfirmation);
+        console.log("  - hasAuthOptions:", this.hasAuthOptions);
+        console.log("  - accountUsername:", this.model.accountUsername);
+      }
 
       if (
         this.model.skipConfirmation ||
@@ -25,7 +36,15 @@ export default apiInitializer("1.8.0", (api) => {
 
       const firstName = this.model.accountUsername.trim().split("_")[0];
 
+      if (settings.enable_debug) {
+        console.log("  - firstName", firstName);
+      }
+
       User.checkUsername(firstName, this.accountEmail).then((result) => {
+        if (settings.enable_debug) {
+          console.log("  checkUsername, result:", result.suggestion);
+        }
+
         if (result.suggestion) {
           this.setProperties({
             accountUsername: result.suggestion,
